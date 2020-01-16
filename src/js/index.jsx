c6aa520ed2d811469504411e41615e8896776f07
Jeremy Kim <thk116@gmail.com>
2020-01-16 11:32:44 -0800

    setup rootStore.js and wrapped the <App/> in a <Provider/> component inside of index.js; to allow React to use the store and call a connect() function

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,5 +1,13 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './app';
+import store from './rootStore';
 
-render(<App />, document.getElementById('root'));
+// eslint-disable-next-line no-undef
+render(
+  <Provider store={ store }>
+    <App />, document.getElementById('root')
+  </Provider>
+);
